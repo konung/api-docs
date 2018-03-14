67ae62153708a6f3f35d5f82e605e83c04813bee
Nick Gorbikoff <nick.gorbikoff@gmail.com>
2018-03-14 11:23:01 -0500

    Adding github-link using updates to torture gem

--- lib/torture_helper.rb
+++ lib/torture_helper.rb
@@ -10,4 +10,25 @@ module TortureHelper
 
     Torture::Snippet.extract_from(file: file, root: root, marker: section, collapse: collapse, unindent: true)
   end
+
+  def github_link(component, section, collapse: "methods", file:nil)
+    config = current_page.data["torture"]
+    config = config[component]
+
+    file ||= config[:default_file]
+    root = config[:root] or raise "No root found for #{component}"
+
+    path_and_line = Torture::Snippet.extract_line_number_and_filename_from(file: file, root: root, marker: section)
+    path = path_and_line[:path].split('/').drop(2).join('/')
+    line = path_and_line[:line]
+    link = Torture::Snippet.build_github_link(github_user: 'trailblazer',
+                                               gem_name: "trailblazer-#{component}",
+                                               path: path,
+                                               line_number: line )
+    markdown_link = "[See code on Github](#{link})"
+  end
+
 end
+
+
+
